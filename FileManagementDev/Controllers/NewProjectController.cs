65fa4ea5a8ea82a1e5d99ac21ae10ccb8cf614d4
Sandeep K <ksandeep36@hotmail.com>
2020-01-25 17:10:42 +0530

    Initial Push - Create project controller

---
+++ FileManagementDev/Controllers/NewProjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileManagementDev.Controllers
+{
+    public class NewProjectController : Controller
+    {
+        // GET: NewProject
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
